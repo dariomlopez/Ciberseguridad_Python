53d19d8f292edce0073f98efb9c930be857dc45e
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-11-07 11:19:05 +0100

    Ejercicios

--- 21_poo.py
+++
@@ -1,4 +0,0 @@
-'''
-Desarrollar un programa que cargue los lados de un triángulo e implemente los siguientes métodos: inicializar los atributos, imprimir el valor del lado mayor y otro método que muestre si es equilátero o no. El nombre de la clase llamarla Triangulo.
-'''
-
---
+++ ejercicios poo/21_poo_triangulo.py
@@ -0,0 +1,8 @@
+'''
+Desarrollar un programa que cargue los lados de un triángulo e implemente los siguientes métodos:
+- inicializar los atributos,
+- imprimir el valor del lado mayor
+- método que muestre si es equilátero o no.
+ nombre de la clase: Triangulo.
+'''
+
