35a7e5a9b6e5a70c16db5ea8d4766da0c505d095
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-11-16 13:22:40 +0100

    Ejercicios

--- ejercicios/8_lista_paises.py
+++ ejercicios/8_lista_paises.py
@@ -1,3 +1,19 @@
 '''
-Crear y cargar en un lista los nombres de 5 países y en otra lista paralela la cantidad de habitantes del mismo. Ordenar alfabéticamente e imprimir los resultados. Por último ordenar con respecto a la cantidad de habitantes (de mayor a menor) e imprimir nuevamente.
+Crear y cargar en un lista los nombres de 5 países y en otra lista paralela la cantidad de habitantes del mismo.
+- Ordenar alfabéticamente
+    e imprimir los resultados.
+- Por último ordenar por cantidad de habitantes (de mayor a menor) e
+    imprimir nuevamente.
 '''
+
+lista_paises, lista_habitantes = [], []
+
+for i in range(5):
+  pais = str(input("Introduce el nombre del pais: "))
+  habitantes = int(input("Introduce el número de habitantes: "))
+  
+  lista_paises.append(pais)
+  lista_habitantes.append(habitantes)
+
+lista_paises.sort(), lista_habitantes.sort()
+print(f"{lista_paises} {lista_habitantes}")
--- ejercicios/9_notas_alumnos.py
+++ ejercicios/9_notas_alumnos.py
@@ -4,3 +4,5 @@ a) Ingresar nombre y nota de cada alumno (almacenar los datos en dos listas para
 b) Realizar un listado que muestre los nombres, notas y condición del alumno. En la condición, colocar "Muy Bueno" si la nota es mayor o igual a 8, "Bueno" si la nota está entre 4 y 7, y colocar "Insuficiente" si la nota es inferior a 4.
 c) Imprimir cuantos alumnos tienen la leyenda “Muy Bueno”.
 '''
+
+lista_nombre, lista_notas = [], []
