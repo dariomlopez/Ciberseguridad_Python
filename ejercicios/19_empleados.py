0ccc79a57fc87a97de03b3424d6c4b90d52730e3
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-11-18 08:41:02 +0100

    Ejercicios

--- ejercicios/19_empleados.py
+++ ejercicios/19_empleados.py
@@ -11,18 +11,30 @@ empleados = [["juan",(2000,3000,4233)] , ["ana",(3444,1000,5333)] ,  etc.   ]
 
 lista_empleados = []
 
-def cargar_nombres():
+def cargar_datos():
   datos_empleado = []
-  nombre = str(input("Introduzca el nombre de empleado: "))
-  datos_empleado.append(nombre)
-  sueldo1 = float(input(f"Introduzca sueldo 1 de {nombre}"))
-  sueldo2 = float(input(f"Introduzca sueldo 2 de {nombre}"))
-  sueldo3 = float(input(f"Introduzca sueldo 3 de {nombre}"))
-  tupla_sueldos = (sueldo1, sueldo2, sueldo3)
-  datos_empleado.append(tupla_sueldos)
-  lista_empleados.append(datos_empleado)
+  for iter in range(5):
+    nombre = str(input("Introduzca el nombre de empleado: "))
+    sueldo1 = float(input(f"Introduzca sueldo 1 de {nombre}"))
+    sueldo2 = float(input(f"Introduzca sueldo 2 de {nombre}"))
+    sueldo3 = float(input(f"Introduzca sueldo 3 de {nombre}"))
+    datos_empleado.append([nombre, (sueldo1, sueldo2, sueldo3)])
+    lista_empleados.append(datos_empleado)
+  return lista_empleados
 
-def cantidad_total():
+def cantidad_total(lista_empleados):
+  for datos in lista_empleados:
+      total = sum(datos[1])
+  print(f"El empleado {datos[0]} cobra por timestre {total}")
 
-
-def ingresos_mayores():
+def ingresos_mayores(lista_empleados):
+  print("Empleados que han cobrado más de 10.000 en el último trimestre")
+  total = 0
+  for datos in lista_empleados:
+      total = sum(datos[1])
+      if total > 10000:
+        print(datos[0], total)
+        
+empleados = cargar_datos()
+cantidad_total(empleados)
+ingresos_mayores(empleados)
