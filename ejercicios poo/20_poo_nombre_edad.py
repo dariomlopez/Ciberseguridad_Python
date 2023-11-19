06efc6c401854dcff089bcf9a663a972fae88ab3
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-11-19 12:14:10 +0100

    Ejercicios

--- ejercicios poo/20_poo_nombre_edad.py
+++ ejercicios poo/20_poo_nombre_edad.py
@@ -1,3 +1,29 @@
 '''
-Confeccionar una clase que permita carga el nombre y la edad de una persona. Mostrar los datos cargados. Imprimir un mensaje si es mayor de edad (edad>=18)
+Confeccionar una clase que permita carga el nombre y la edad de una persona. Mostrar los datos cargados.
+
+- Imprimir un mensaje si es mayor de edad (edad>=18)
 '''
+
+class Datos:
+  def __init__(self, nombre, edad):
+    self.nombre = nombre
+    self.edad = edad
+    # nombre = str(input("Introduce un nombre"))
+    # edad = int(input("introduce una edad"))
+  def imprimir(self):
+    print(f"Nombre: {self.nombre} - {self.edad}")
+  
+  def mayor18(self):
+    if self.edad > 18:
+      print(f"{self.nombre} es mayor de edad")
+    else:
+      print(f"{self.nombre} es menor de edad")
+      
+
+persona1 = Datos("Pepe", 14)
+persona2 = Datos("Ramón", 41)
+persona3 = Datos("Iñaki", 22)
+
+persona1.imprimir(), persona1.mayor18()
+persona2.imprimir(), persona2.mayor18()
+persona3.imprimir(), persona3.mayor18()
