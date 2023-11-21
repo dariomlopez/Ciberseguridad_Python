daf7bce1256a7387506363b60303763e61527caa
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-11-21 09:08:35 +0100

    Ejercicios

--- 32_ficheros.py
+++
@@ -1,7 +0,0 @@
-'''
-32.- Generar tantos archivos como letras del abecedario, todos con formato txt.
-'''
-
-for char in 'abcdefghijklmnopqrstuvwxyz':
-  with open(f"{char}.txt", "w") as file:
-    file.write(str(char))
