15bf8ad0bc3d3eaf27b6088555f1aa47b28718bb
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-11-25 21:25:50 +0100

    Ejercicios

---
+++ ejercicios_tkinter/ejercicio40.py
@@ -0,0 +1,6 @@
+'''
+40.- Implementar 3 controles de tipo Radiobutton con las etiquetas 'Rojo', 'Verde' y 'Azul'. Cuando se presione un botón cambiar el color de fondo del formulario. Si consideramos que la variable ventana1 es un objeto de la clase Tk, si queremos que el fondo sea de color rojo debemos llamar al método configure y en el parámetro bg indicar un string con el color a activar ("red", "green" o "blue"): bg="red"
+'''
+
+import tkinter as tkinter
+
